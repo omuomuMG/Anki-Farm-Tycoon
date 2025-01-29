fe10b3e226120dd7ff9b5fdd9d191356a68e6428
omuomuMG <omuomunotrice@gmail.com>
2025-01-29 17:34:42 +0900

    refact: add game window to init

--- __init__.py
+++ __init__.py
@@ -2,14 +2,15 @@ from aqt import mw, gui_hooks
 from aqt.qt import *
 from aqt.utils import showInfo
 
-from .utils import game_window
+from aqt import mw
+from .gui.game_widget import GameWidget
+
+def game_window():
+    mw.myWidget = widget = GameWidget()
+    widget.show()
 
 action = QAction("Ranch launch", mw)
 action.triggered.connect(game_window)
 
 
-
-
-
-# Ankiのツールメニューにアクションを追加
 mw.form.menuTools.addAction(action)
