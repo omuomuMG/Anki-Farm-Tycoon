77e19921e75560a6f9726fc5a525dc853de414aa
omuomuMG <omuomunotrice@gmail.com>
2025-05-22 20:46:25 +0900

    Feature: Create user data

--- gui/leaderboard.py
+++ gui/leaderboard.py
@@ -2,6 +2,7 @@ from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLabel, QListWidget
 import urllib.request
 import urllib.parse
 import json
+import datetime
 
 SUPABASE_URL = "https://cbucgwfitimxvsayzpsa.supabase.co"
 SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNidWNnd2ZpdGlteHZzYXl6cHNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NTMwODksImV4cCI6MjA2MzIyOTA4OX0.Roosk0WMD2SG27iN-b_Shnge-wPlsgHNRXhflQRw2BQ"
@@ -61,6 +62,29 @@ def update_user_data(user_name = "omuomu", update_fields = {"money":10101}):
 
     data = json.dumps(update_fields).encode("utf-8")
     req = urllib.request.Request(url, data=data, headers=headers, method="PATCH")
+
+    try:
+        with urllib.request.urlopen(req) as response:
+            result = response.read().decode()
+            return json.loads(result)
+    except urllib.error.HTTPError as e:
+        print(f"HTTP Error: {e.code} - {e.reason}")
+        return None
+    
+
+
+def create_user_data(user_fields = {"id":5,"name":"まーごめ", "password":"margome", "money":100,"createdAt":datetime.datetime.now().strftime("%Y-%m-%d")}):
+    print("create User data-------")
+    url = f"{SUPABASE_URL}/rest/v1/users"
+    headers = {
+        "apikey": SUPABASE_API_KEY,
+        "Authorization": f"Bearer {SUPABASE_API_KEY}",
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Prefer": "return=representation"
+    }
+    data = json.dumps(user_fields).encode("utf-8")
+    req = urllib.request.Request(url, data=data, headers=headers, method="POST")
 
     try:
         with urllib.request.urlopen(req) as response:
