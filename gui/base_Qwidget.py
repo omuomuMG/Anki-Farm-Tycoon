4d289206a2e9220e22c6c7c5079b450ae29c46eb
unknown <66956954+YISSSHJIAAA@users.noreply.github.com>
2026-03-29 22:28:11 +0800

    Create base_Qwidget.py
    | Class                      | Inherits From          | Usage            |
    | -------------------------- | ---------------------- | ---------------- |
    | `GameWidget`               | `QWidget`              | Docked main game |
    | `ShopWindow`               | `QDialog`  | Modal popup      |
    | `EmployeeManagementWindow` | `QDialog`  | Modal popup      |
    | `StatisticsWindow`         | `QDialog` | Modal popup      |
    | `LeaderBoardWindow`        | `QDialog`  | Modal popup      |

---
+++ gui/base_Qwidget.py
@@ -0,0 +1,19 @@
+from PyQt6.QtWidgets import QWidget
+class BaseWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.parent = parent
+        self.button_connections = {}
+        self.setup_ui()
+
+    def setup_ui(self):
+        pass
+
+    def register_button(self, button, handler):
+        if button in self.button_connections:
+            button.clicked.disconnect(self.button_connections[button])
+
+        wrapper = lambda checked, h=handler: h()
+
+        button.clicked.connect(wrapper)
+        self.button_connections[button] = wrapper
